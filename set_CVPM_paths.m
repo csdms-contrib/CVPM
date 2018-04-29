e9e8e05c8c8d9498d6109dfb7baba603328b6d3e
Gary Clow <33873878+garyclow@users.noreply.github.com>
2018-04-29 14:16:43 -0600

    Add files via upload

--- set_CVPM_paths.m
+++ set_CVPM_paths.m
@@ -1,17 +1,35 @@
 % set_CVPM_paths.m
 
-% Sets the paths to required CVPM directories.
-% ____________________________________________
+% Sets the required paths for CVPM directories.
+% ______________________________________________
 
-% > Set CVPM working directory
+%	Copyright (C) 2018, Gary Clow
 
-% *** change the following statement to satisfy your working environment ***
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, version 3 of the License.
+
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License v3.0 for more details.
+
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%	Developer can be contacted by,
+%	email at:
+%		gary.clow@colorado.edu
+%	paper mail at:
+%		Institute of Arctic and Alpine Research
+%		University of Colorado
+%		Campus Box 450
+%		Boulder, CO 80309-0450 USA
+% ______________________________________________
+
+% Set CVPM working directory
 
  wdir = '~/thermal/numer/CVPM_v1.1';
-
-% ************************************
-
-% > Add paths
 
  addpath( wdir,                '-end')
  addpath([wdir '/AK_projects'],'-end')
